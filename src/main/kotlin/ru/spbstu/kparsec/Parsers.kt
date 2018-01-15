e8c73427c88be263c9563d6b49267e948b530145
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-01-15 19:39:40 +0300

    Some KDocs

--- src/main/kotlin/ru/spbstu/kparsec/Parsers.kt
+++ src/main/kotlin/ru/spbstu/kparsec/Parsers.kt
@@ -1,9 +1,34 @@
 package ru.spbstu.kparsec
 
+/**
+ * The basic parser interface.
+ *
+ * @param T the type of input tokens (Char for simple cases)
+ * @param R the type of result of parsing
+ */
 interface Parser<T, out R> {
+    /**
+     * Perform parsing on [input]
+     * @see ParseResult
+     * @return the result of parsing
+     */
     operator fun invoke(input: Input<T>): ParseResult<T, R>
 }
 
+/**
+ * Parse a string
+ * @see StringInput
+ */
 fun<T> Parser<Char, T>.parse(string: String): ParseResult<Char, T> = this(StringInput(string))
+
+/**
+ * Parse a list of tokens
+ * @see ListInput
+ */
 fun<T, E> Parser<T, E>.parse(data: List<T>): ParseResult<T, E> = this(ListInput(data))
+
+/**
+ * Parse an array of tokens
+ * @see ListInput
+ */
 fun<T, E> Parser<T, E>.parse(data: Array<T>): ParseResult<T, E> = this(ListInput(data.asList()))
--- src/main/kotlin/ru/spbstu/kparsec/Result.kt
+++ src/main/kotlin/ru/spbstu/kparsec/Result.kt
@@ -2,10 +2,36 @@ package ru.spbstu.kparsec
 
 import kotlinx.Warnings
 
+/**
+ * The result of parsing
+ * @param T the type of token (*Char* for strings)
+ * @param R the actual result type
+ * @see Success
+ * @see Failure
+ */
 sealed class ParseResult<out T, out R>
+
+/**
+ * Successful parse result
+ * @param T the type of token (*Char* for strings)
+ * @param R the actual result type
+ * @property rest the rest of input
+ * @property result the actual result
+ */
 data class Success<out T, out R>(val rest: Input<T>, val result: R): ParseResult<T, R>()
+
+/**
+ * Parse result representing failure
+ * @property expected string representation of what was expected here
+ * @property location current source location
+ */
 data class Failure(val expected: String, val location: Location): ParseResult<Nothing, Nothing>()
 
+/**
+ * Transform the result value using a function [f]
+ * @return [Success] with current result if was a [Success]
+ * @return [this] if result was a [Failure]
+ */
 @Suppress(Warnings.NOTHING_TO_INLINE)
 inline fun<T, R, S> ParseResult<T, R>.map(f: (R) -> S): ParseResult<T, S> = when(this) {
     is Success -> Success(rest, result = f(result))
